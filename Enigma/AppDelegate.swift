5543df37f3f5bd7cccd648e219bce367e7099714
Paul Williamson <squarefrog@gmail.com>
2016-04-26 17:35:57 +0100

    Create empty Xcode project

---
+++ Enigma/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+}
+
