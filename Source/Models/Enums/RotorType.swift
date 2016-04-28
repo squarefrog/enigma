43a03d4b13a49de4c703369a70263e6e9f0c7e84
Paul Williamson <squarefrog@gmail.com>
2016-04-28 14:19:31 +0100

    Add RotorType enum

---
+++ Source/Models/Enums/RotorType.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// swiftlint:disable type_name
+enum RotorType: String {
+    case I
+    case II
+    case III
+    case IV
+    case V
+    case VI
+    case VII
+    case VIII
+}
